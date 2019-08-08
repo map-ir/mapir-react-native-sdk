91af827fcb90053fdee3fc7f58d59e40e474a44a
Reyhane Masumi <R.Masumi94@gmail.com>
2019-08-08 14:02:59 +0430

    README.md

--- src/index.js
+++ src/index.js
@@ -7,15 +7,12 @@ import ShapeSource from './../components/shapeSource';
 import LineLayer from './../components/lineLayer';
 import RasterSource from './../components/RasterSource';
 import RasterLayer from './../components/RasterLayer';
-<<<<<<< HEAD
-=======
 import Popup from "./../components/popup";
 import Camera from "./../components/Camera";
 import SymbolLayer from "./../components/SymbolLayer";
 import UserLocation from './../components/UserLocation';
 import Images from './../components/Images';
 import FillLayer from "./../components/fillLayer";
->>>>>>> 49b2232f754cb9352080c43978f6254263fc0cd0
 
 const instructions = Platform.select({
     ios: true,
@@ -32,21 +29,12 @@ class Mapir extends Component<> {
     }
 
     render() {
-<<<<<<< HEAD
-        Mapbox.apiKey(this.props.accessToken)
-        Mapbox.StyleURL.Street = `https://map.ir/vector/styles/main/main_mobile_style.json?x-api-key=${this.props.accessToken}`;
-        return (
-            <Mapbox.MapView
-                {...this.props}
-                styleURL={Mapbox.StyleURL.Street}
-=======
         if (Platform === 'android') {
             Mapbox.apiKey(this.props.accessToken)
         }
         return (
             <Mapbox.MapView
                 {...this.props}
->>>>>>> 49b2232f754cb9352080c43978f6254263fc0cd0
                 attributionEnabled={false}
                 logoEnabled={instructions}
                 maxZoomLevel={20}
@@ -61,14 +49,12 @@ Mapir.ShapeSource = ShapeSource
 Mapir.LineLayer = LineLayer
 Mapir.RasterSource = RasterSource
 Mapir.RasterLayer = RasterLayer
-<<<<<<< HEAD
-=======
 Mapir.Popup = Popup
 Mapir.Camera = Camera
 Mapir.SymbolLayer = SymbolLayer
 Mapir.UserLocation = UserLocation
 Mapir.Images = Images
 Mapir.FillLayer = FillLayer
->>>>>>> 49b2232f754cb9352080c43978f6254263fc0cd0
 
 export default Mapir
+
